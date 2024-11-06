b62c041f10e6418402a8cf882680b0634dfdfac3
MUHAMMAD REHAN ALAM <alamrehan1234@gmail.com>
2024-11-06 06:21:13 +0530

    update

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the Firebase Admin SDK to access Firebase services
 const admin = require('firebase-admin');
 
-// Import the Firebase credentials (the JSON file you downloaded)
+// Import the Firebase credentials
 const serviceAccount = {
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
@@ -11,7 +11,7 @@ const serviceAccount = {
 // Initialize the Firebase app using the credentials
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com` // Replace 'your-project-id' with your Firebase project ID
+    storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`
 });
 
 // Get a reference to the Firebase Storage bucket
